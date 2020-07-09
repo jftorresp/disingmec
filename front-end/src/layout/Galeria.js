630a7688bfea115e10e5ce968e8a4bb9274759da
Juan Felipe Torres <jftorresp@MacBook-Pro-de-Juan.local>
2020-07-09 13:57:12 -0500

    gallery added

--- front-end/src/layout/Galeria.js
+++ front-end/src/layout/Galeria.js
@@ -1,11 +1,27 @@
 import React from "react";
+import ImageGallery from "react-image-gallery";
 
 const Galeria = () => {
+  const images = [
+    {
+      original: "https://picsum.photos/id/1018/1000/600/",
+      thumbnail: "https://picsum.photos/id/1018/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1015/1000/600/",
+      thumbnail: "https://picsum.photos/id/1015/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1019/1000/600/",
+      thumbnail: "https://picsum.photos/id/1019/250/150/",
+    },
+  ];
   return (
     <div className="Galeria">
       <div className="curve" id="curve-gal">
         <div className="container">
           <h2>Galeria</h2>
+          <ImageGallery items={images} />
         </div>
       </div>
     </div>
